d4e52f6566d4bb58c5dc685574bc681841f6b10d
crantz007 <42812015+crantz007@users.noreply.github.com>
2020-08-24 03:10:05 -0500

    Add files via upload
    C# adding combobox

---
+++ To-Do-List/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace To_Do_List
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clsToDo = new System.Windows.Forms.CheckedListBox();
+            this.lstDone = new System.Windows.Forms.ListBox();
+            this.txtNewToDo = new System.Windows.Forms.TextBox();
+            this.btnAddToDo = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkUrgent = new System.Windows.Forms.CheckBox();
+            this.cBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // clsToDo
+            // 
+            this.clsToDo.FormattingEnabled = true;
+            this.clsToDo.Location = new System.Drawing.Point(28, 97);
+            this.clsToDo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.clsToDo.Name = "clsToDo";
+            this.clsToDo.Size = new System.Drawing.Size(592, 169);
+            this.clsToDo.TabIndex = 0;
+            // 
+            // lstDone
+            // 
+            this.lstDone.FormattingEnabled = true;
+            this.lstDone.Location = new System.Drawing.Point(28, 406);
+            this.lstDone.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.lstDone.Name = "lstDone";
+            this.lstDone.Size = new System.Drawing.Size(589, 82);
+            this.lstDone.TabIndex = 1;
+            // 
+            // txtNewToDo
+            // 
+            this.txtNewToDo.Location = new System.Drawing.Point(25, 24);
+            this.txtNewToDo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNewToDo.Name = "txtNewToDo";
+            this.txtNewToDo.Size = new System.Drawing.Size(228, 20);
+            this.txtNewToDo.TabIndex = 2;
+            this.txtNewToDo.TextChanged += new System.EventHandler(this.txtNewToDo_TextChanged);
+            // 
+            // btnAddToDo
+            // 
+            this.btnAddToDo.Location = new System.Drawing.Point(537, 17);
+            this.btnAddToDo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAddToDo.Name = "btnAddToDo";
+            this.btnAddToDo.Size = new System.Drawing.Size(80, 23);
+            this.btnAddToDo.TabIndex = 3;
+            this.btnAddToDo.Text = "Add";
+            this.btnAddToDo.UseVisualStyleBackColor = true;
+            this.btnAddToDo.Click += new System.EventHandler(this.btnAddToDo_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(28, 283);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(194, 28);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Remove checked items";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "What do you need to do?";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 82);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Things to do";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 385);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Deleted Items";
+            // 
+            // chkUrgent
+            // 
+            this.chkUrgent.AutoSize = true;
+            this.chkUrgent.Location = new System.Drawing.Point(406, 24);
+            this.chkUrgent.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chkUrgent.Name = "chkUrgent";
+            this.chkUrgent.Size = new System.Drawing.Size(64, 17);
+            this.chkUrgent.TabIndex = 8;
+            this.chkUrgent.Text = "Urgent?";
+            this.chkUrgent.UseVisualStyleBackColor = true;
+            this.chkUrgent.CheckedChanged += new System.EventHandler(this.chkUrgent_CheckedChanged);
+            // 
+            // cBox
+            // 
+            this.cBox.FormattingEnabled = true;
+            this.cBox.Location = new System.Drawing.Point(270, 22);
+            this.cBox.Name = "cBox";
+            this.cBox.Size = new System.Drawing.Size(121, 21);
+            this.cBox.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(282, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Category";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnAddToDo;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 389);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cBox);
+            this.Controls.Add(this.chkUrgent);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAddToDo);
+            this.Controls.Add(this.txtNewToDo);
+            this.Controls.Add(this.lstDone);
+            this.Controls.Add(this.clsToDo);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox clsToDo;
+        private System.Windows.Forms.ListBox lstDone;
+        private System.Windows.Forms.TextBox txtNewToDo;
+        private System.Windows.Forms.Button btnAddToDo;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkUrgent;
+        private System.Windows.Forms.ComboBox cBox;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ To-Do-List/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace To_Do_List
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            List<string> ct = new List<string>();
+            ct.Add("Personal");
+            ct.Add("Work");
+            ct.Add("School");
+
+            cBox.Items.AddRange(ct.ToArray());
+            
+        }
+
+        private void btnAddToDo_Click(object sender, EventArgs e)
+        {
+            // Remove any spaces start and end of string
+            string newItem = txtNewToDo.Text.Trim();
+            List<string> ct = new List<string>();
+            ct.Add("Personal");
+            ct.Add("Work");
+            ct.Add("School");
+
+            cBox.Items.AddRange(ct.ToArray());
+
+            if (!String.IsNullOrWhiteSpace(newItem))
+            {
+                if (itemIsInList(clsToDo.Items, newItem))
+                {
+                    MessageBox.Show("Duplicate item", "Warning");
+                }
+                else
+                {
+                    // Use Add to add new item to end of Item collection
+                    DateTime todoCreated = DateTime.Now;
+                    bool urgent = chkUrgent.Checked;
+                    var  category = this.cBox.GetItemText(this.cBox.SelectedItem);
+
+                    // Format the text, date/time created and urgent into one string
+                    string todoText = $"{newItem} - {category} - Created at {todoCreated:g}";
+                    if (urgent)
+                    {
+                        todoText += " URGENT!";
+                    }
+                    // add to the ListBox items
+                    clsToDo.Items.Add(todoText);
+                    cBox.Items.Add(category);
+
+
+                    // Clear inputs
+                    txtNewToDo.Text = "";
+                    chkUrgent.Checked = false;
+                }
+                // No else, just ignore empty input
+            }
+        }
+
+        private bool itemIsInList(CheckedListBox.ObjectCollection items, string newItem)
+        {
+           foreach(string item in items)
+            {
+                if (item.ToUpper() == newItem.ToUpper())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // Make new list 
+            List<string> doneItems = new List<string>();
+
+            // Copy all checked item to new list
+            foreach (string item in clsToDo.CheckedItems)
+            {
+                doneItems.Add(item);
+
+            }
+
+            // For each string in doneItems list, remove from clsToDo.Items
+            // Add to lstDone.Items
+            foreach (string item in doneItems)
+            {
+                clsToDo.Items.Remove(item);  // Remove by value
+                cBox.Items.Remove(cBox.SelectedItem);
+                lstDone.Items.Add(item);
+            }
+            
+        }
+
+        private void chkUrgent_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNewToDo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
